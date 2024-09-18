590ccdc38cc69f18215494855eea08651c5a3cd7
Renata Ferreira Martins Rocha <renata.rocha@e-core.com>
2024-09-18 14:02:06 -0300

    fix: change get role by id http method from post to get; merge GET /v1/roles and GET /v1/roles?userId={userId}&teamId={teamId} controllers

--- src/main/java/com/ecore/roles/web/rest/RolesRestController.java
+++ src/main/java/com/ecore/roles/web/rest/RolesRestController.java
@@ -1,5 +1,6 @@
 package com.ecore.roles.web.rest;
 
+import com.ecore.roles.exception.RequiredParameterException;
 import com.ecore.roles.model.Role;
 import com.ecore.roles.service.RolesService;
 import com.ecore.roles.web.RolesApi;
@@ -34,26 +35,35 @@ public class RolesRestController implements RolesApi {
     }
 
     @Override
-    @GetMapping(
-            produces = {"application/json"})
-    public ResponseEntity<List<RoleDto>> getRoles() {
-
-        List<Role> getRoles = rolesService.GetRoles();
-
+    @GetMapping
+    public ResponseEntity<List<RoleDto>> getRoles(
+            @RequestParam(required = false) UUID userId,
+            @RequestParam(required = false) UUID teamId) {
         List<RoleDto> roleDtoList = new ArrayList<>();
 
-        for (Role role : getRoles) {
-            RoleDto roleDto = fromModel(role);
-            roleDtoList.add(roleDto);
+        if (userId == null && teamId == null) {
+            List<Role> getRoles = rolesService.GetRoles();
+            for (Role role : getRoles) {
+                RoleDto roleDto = fromModel(role);
+                roleDtoList.add(roleDto);
+            }
+        } else {
+            if (userId == null) {
+                throw new RequiredParameterException("userId");
+            } else if (teamId == null) {
+                throw new RequiredParameterException("teamId");
+            } else {
+                RoleDto roleDto = fromModel(rolesService.GetRole(userId, teamId));
+                roleDtoList.add(roleDto);
+            }
         }
-
         return ResponseEntity
                 .status(200)
                 .body(roleDtoList);
     }
 
     @Override
-    @PostMapping(
+    @GetMapping(
             path = "/{roleId}",
             produces = {"application/json"})
     public ResponseEntity<RoleDto> getRole(
@@ -61,16 +71,6 @@ public class RolesRestController implements RolesApi {
         return ResponseEntity
                 .status(200)
                 .body(fromModel(rolesService.GetRole(roleId)));
-    }
-
-    @Override
-    @GetMapping
-    public ResponseEntity<RoleDto> getRole(
-            @RequestParam UUID userId,
-            @RequestParam UUID teamId) {
-        return ResponseEntity
-                .status(200)
-                .body(fromModel(rolesService.GetRole(userId, teamId)));
     }
 
 }
