d9e46039121e3544ca1cc03f2dfa4ce54852a558
Renata Ferreira Martins Rocha <renata.rocha@e-core.com>
2024-09-19 09:54:51 -0300

    implement verifyThatUserBelongsToTeam in TeamsServiceImpl

--- src/main/java/com/ecore/roles/service/impl/TeamsServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/TeamsServiceImpl.java
@@ -2,11 +2,13 @@ package com.ecore.roles.service.impl;
 
 import com.ecore.roles.client.TeamsClient;
 import com.ecore.roles.client.model.Team;
+import com.ecore.roles.exception.ResourceNotFoundException;
 import com.ecore.roles.service.TeamsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,11 +21,19 @@ public class TeamsServiceImpl implements TeamsService {
         this.teamsClient = teamsClient;
     }
 
-    public Team getTeam(UUID id) {
-        return teamsClient.getTeam(id).getBody();
+    public Optional<Team> getTeam(UUID id) {
+        return Optional.ofNullable(teamsClient.getTeam(id).getBody());
     }
 
     public List<Team> getTeams() {
         return teamsClient.getTeams().getBody();
+    }
+
+    public boolean verifyThatUserBelongsToTeam(UUID userId, UUID teamId) {
+        Optional<Team> team = getTeam(teamId);
+        if (team.isPresent()) {
+            return team.get().getTeamMemberIds().stream().anyMatch(i -> i.equals(userId));
+        }
+        throw new ResourceNotFoundException(Team.class, teamId);
     }
 }
