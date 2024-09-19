a8bcada1f1a3c22edd6c55b9f3e9741e5b59ff67
Renata Ferreira Martins Rocha <renata.rocha@e-core.com>
2024-09-19 17:47:30 -0300

    fix RolesApi interface to reflect modifications in RolesRestController

--- src/main/java/com/ecore/roles/web/RolesApi.java
+++ src/main/java/com/ecore/roles/web/RolesApi.java
@@ -11,7 +11,9 @@ public interface RolesApi {
     ResponseEntity<RoleDto> createRole(
             RoleDto role);
 
-    ResponseEntity<List<RoleDto>> getRoles(
+    ResponseEntity<List<RoleDto>> getAllRoles();
+
+    ResponseEntity<RoleDto> searchRole(
             UUID userId,
             UUID teamId);
 
